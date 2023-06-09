4dd47f917db47dbf36da5fc6553b2dc18d08af51
rik beernink <rikbeernink@mailbox.org>
2023-06-09 18:18:55 +0200

    Accidentally removed end tag </option>

--- nuxt3/components/caoAccordion.vue
+++ nuxt3/components/caoAccordion.vue
@@ -6,7 +6,7 @@
 
     <div class="form-group">
       <select class="form-select" aria-label="category" required v-model="category">
-        <option value="">Alle Categorieën/option>
+        <option value="">Alle Categorieën</option>
         <option value="WRR">WRR</option>
         <option value="Salaris">Salaris</option>
         <option value="Vakantie">Vakantie</option>
