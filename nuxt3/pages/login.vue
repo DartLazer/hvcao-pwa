901ee4acfaee9f74023cb08335de48488ce7d6b9
rik beernink <rikbeernink@mailbox.org>
2023-06-09 04:11:59 +0200

    Changed meehelpen tekst at admin login page

--- nuxt3/pages/login.vue
+++ nuxt3/pages/login.vue
@@ -1,18 +1,38 @@
 <template>
   <div class="container">
     <form @submit.prevent="login" class="form-signin">
-      <h2 class="form-signin-heading">Please sign in</h2>
+      <h2 class="form-signin-heading">Inloggen</h2>
       <div class="form-group">
-        <input type="text" class="form-control" placeholder="Username" required autofocus v-model="username">
+        <input type="text" class="form-control" placeholder="Gebruikersnaam" required autofocus v-model="username">
       </div>
       <div class="form-group">
-        <input type="password" class="form-control" placeholder="Password" required v-model="password">
+        <input type="password" class="form-control" placeholder="Wachtwoord" required v-model="password">
       </div>
       <div v-if="message">
         <p class="alert alert-info mt-2">{{ message }}</p>
       </div>
       <button class="btn btn-lg btn-primary btn-block mt-3" type="submit">Sign in</button>
     </form>
+
+    <div class="accordion mt-5" id="accordionExample">
+      <div class="accordion-item">
+        <h2 class="accordion-header" id="headingTwo">
+          <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                  data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+            Mee Helpen?
+          </button>
+        </h2>
+        <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo"
+             data-bs-parent="#accordionExample">
+          <div class="accordion-body">
+            <strong>We zijn op zoek naar database-auteurs!</strong> Dit project wordt vrijwillig onderhouden door
+            collega's binnen Transavia vanuit de cockpit. Als je een goede kennis hebt van de CAO en andere randzaken en
+            graag wilt bijdragen, <a href="https://wa.me/31652500397" class="link-secondary">stuur me dan een berichtje
+            via WhatsApp!</a>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
