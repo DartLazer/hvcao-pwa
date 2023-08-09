116e19df411acbe6844bb334f93a6c45588a6c3e
Rik Beernink <rikbeernink@mailbox.org>
2023-08-09 20:37:07 +0200

    Added contact section to about and reformatting.

--- nuxt3/pages/about.vue
+++ nuxt3/pages/about.vue
@@ -2,22 +2,29 @@
   <div>
     <div class="container  col-md-auto rounded-1">
       <h1>Welkom bij de Transavia CAO FAQ Webapp</h1>
-      <div class="rounded-2 p-3 pb-5 pt-3 text-black" style="background: RGBA(248, 249, 250, 0.9)">
-        <p>Deze webapp, nog volop in ontwikkeling, streeft ernaar om een uitgebreide database te worden met veelgestelde
-          vragen over het Transavia cao. Dit zijn vragen die regelmatig leven binnen ons vliegerskorps.</p>
-        <p>De webapp ontwikkeld door <a class="link-secondary" href="https://www.sky-t.nl">Rik Beernink
-          (Sky-T)</a>, de maker van o.a. <a href="www.vluchtjeruilen.nl" class="link-secondary">vluchtjeruilen.nl</a>.
-          De vragen database wordt zorgvuldig onderhouden door meerdere
-          collega's van Transavia.</p>
-        <p>We zijn op zoek naar collega's met diepgaande kennis van het CAO en sterke schrijfvaardigheden om bij te
-          dragen aan het opzetten en onderhouden van de vragen database. Als je wat tijd over hebt en graag een bijdrage
-          wilt leveren, <a class="link-secondary" href="https://wa.me/31652500397">neem dan contact met ons op via
-            WhatsApp!</a>
-          We kijken ernaar uit om samen te werken.</p>
-        <br/>
-        <br/>
-        Deze web-app is geschreven met Nuxt3, Vuejs3, Django en Docker. Als je wilt mee helpen of benieuwd bent naar de
-        code <a href="https://github.com/DartLazer/hvcao-pwa" class="link-secondary">klik dan hier!</a>
+      <div class="rounded-2 p-3  text-black" style="background: RGBA(248, 249, 250, 0.9)">
+        <div class="pb-3 pt-3">
+          <p>Deze webapp, nog volop in ontwikkeling, streeft ernaar om een uitgebreide database te worden met
+            veelgestelde
+            vragen over het Transavia cao. Dit zijn vragen die regelmatig leven binnen ons vliegerskorps.</p>
+          <p>De webapp is ontwikkeld door <a class="link-secondary" href="https://www.sky-t.nl">Rik Beernink
+            (Sky-T)</a>, de maker van o.a. <a href="www.vluchtjeruilen.nl" class="link-secondary">vluchtjeruilen.nl</a>.
+            De vragen database wordt zorgvuldig onderhouden door meerdere
+            collega's van Transavia.</p>
+          <p>We zijn op zoek naar collega's met diepgaande kennis van het CAO en sterke schrijfvaardigheden om bij te
+            dragen aan het opzetten en onderhouden van de vragen database. Als je wat tijd over hebt en graag een
+            bijdrage
+            wilt leveren, <a class="link-secondary" href="https://wa.me/31652500397">neem dan contact met ons op via
+              WhatsApp!</a></p>
+          <p>
+            We kijken ernaar uit om samen te werken.</p>
+          <h5 class="pt-3">Vragen, opmerkingen of suggesties?</h5>
+          <p>Altijd welkom! <a class="link-secondary" href="https://wa.me/31652500397">Stuur mij even een whatsapp
+            bericht</a> en dan kom ik zo snel mogelijk bij je terug!</p>
+        </div>
+        <p class="small">Deze web-app is geschreven met Nuxt3, Vuejs3, Django en Docker. Als je mee wilt helpen of
+          benieuwd bent naar de
+          code <a href="https://github.com/DartLazer/hvcao-pwa" class="link-secondary">klik dan hier!</a></p>
       </div>
     </div>
   </div>
