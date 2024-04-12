e70484012ce90b97755df83747540f79764a856d
DartLazer <71255041+DartLazer@users.noreply.github.com>
2024-04-12 10:26:37 +0200

    Update pensioencalculator.vue. No more discrepancies

--- nuxt3/pages/pensioencalculator.vue
+++ nuxt3/pages/pensioencalculator.vue
@@ -4,9 +4,14 @@
       <h1>Pensioenbijdrage Calculator</h1>
       <div class="rounded-2 p-3 text-black" style="background: RGBA(248, 249, 250, 0.9)">
         <p class="col-lg-8">
-          Deze tool is ontworpen om je inzicht te geven in hoeveel van je salaris je <strong>netto</strong> apart zou
-          moeten zetten voor je pensioen, gebaseerd op je huidige salarisschaal, om te voldoen aan de bedachte
-          pensioensopbouw zoals bedoeld in het CAO.
+          Deze tool is ontwikkeld om inzichtelijk te maken welk deel van het netto bedrag dat je van je werkgever
+          ontvangt reeds bestemd is voor je pensioen, conform de richtlijnen in je CAO.
+        </p>
+        <p class="col-lg-8">
+          Dit is geen advies over hoeveel
+          je zou moeten sparen/beleggen, maar een weergave van het deel van je nettoloon dat volgens de CAO voor je
+          pensioen is
+          gereserveerd.
         </p>
         <form @submit.prevent="calculateContribution">
           <div class="mb-3">
@@ -20,14 +25,14 @@
             <label for="salaryScale" class="form-label">Salarisschaal</label>
             <select id="salaryScale" class="form-select" v-model="formData.salaryScale">
               <option v-for="scale in 32" :key="scale" :value="scale">Schaal {{ scale }} (€
-                {{ getSalaryByFunctionAndScale(formData.function, scale) }})
+                {{ formatNumber(getSalaryByFunctionAndScale(formData.function, scale)) }})
               </option>
             </select>
           </div>
           <div class="mb-3">
             <label for="age" class="form-label">Leeftijd (die je bereikt in dit kalenderjaar)</label>
             <select id="age" class="form-select" v-model="formData.age">
-              <option v-for="age in 37" :key="age" :value="age + 20">{{ age + 20 }}</option>
+              <option v-for="age in 37" :key="age" :value="age + 20">{{ age + 21 }}</option>
             </select>
           </div>
           <div class="mb-3">
@@ -42,20 +47,23 @@
         </form>
 
         <div class="fw-light small text-muted mt-2">
-          <p>Disclaimer: De informatie en data die door deze tool en deze pagina worden worden verstrekt, zijn uitsluitend bedoeld voor
+          <p>Disclaimer: De informatie en data die door deze tool en deze pagina worden worden verstrekt, zijn
+            uitsluitend bedoeld voor
             informatieve doeleinden. Let op: De verstrekte informatie mag niet worden gezien als financieel advies,
             investeringsadvies of enige vorm van aanbeveling. Wij kunnen niet verantwoordelijk worden gehouden door
             eventuele onjuistheden in deze informatie of voor de gevolgen van beslissingen die genomen worden op basis
             van deze informatie. </p>
         </div>
 
-        <div v-if="showResults" class="mt-3">
+        <div v-if="showResults" ref="resultsRef" class="mt-3">
 
           <buy-me-coffee/>
 
 
           <h5>Berekende Pensioenbijdrage</h5>
-          <p>Op basis van de ingevoerde informatie zou je maandelijks <strong>€{{ calculatedContribution.toFixed(2) }}
+          <p>Op basis van de ingevoerde informatie zou je maandelijks <strong>€{{
+              formatNumber(calculatedContribution)
+            }}
             netto</strong>
             apart moeten zetten voor je pensioen.</p>
 
@@ -72,19 +80,19 @@
               <tbody>
               <tr>
                 <th scope="row" class="fw-normal">Bruto Toeslag</th>
-                <td>€{{ brutoToeslag.toFixed(2) }}</td>
+                <td>€{{ formatNumber(brutoToeslag) }}</td>
               </tr>
               <tr>
                 <th scope="row" class="fw-normal">Aanvulling Bruto Toeslag</th>
-                <td>€{{ (netoBrutoToeslag.toFixed(2) - brutoToeslag.toFixed(2)).toFixed(2) }}</td>
+                <td>€{{ formatNumber((netoBrutoToeslag - brutoToeslag)) }}</td>
               </tr>
               <tr>
                 <th scope="row" class="fw-normal">Staffel 1</th>
-                <td>€{{ staffel_1.toFixed(2) }}</td>
+                <td>€{{ formatNumber(staffel_1) }}</td>
               </tr>
               <tr>
                 <th scope="row" class="fw-normal">Staffel 2</th>
-                <td>€{{ staffel_2.toFixed(2) }}</td>
+                <td>€{{ formatNumber(staffel_2) }}</td>
               </tr>
               <!-- Empty row for spacing -->
               <tr>
@@ -92,22 +100,22 @@
               </tr>
               <tr>
                 <th scope="row" class="fw-normal">Totaal Bruto</th>
-                <td>€{{ (netoBrutoToeslag + staffel_1 + staffel_2).toFixed(2) }}</td>
+                <td>€{{ formatNumber(totalBruto) }}</td>
               </tr>
               <tr>
                 <th scope="row" class="">Totaal Netto</th>
-                <td>€{{ calculatedContribution.toFixed(2) }}</td>
+                <td>€{{ formatNumber(calculatedContribution) }}</td>
               </tr>
               </tbody>
             </table>
             <p class="fw-light">
               Op je loonstrookje staan deze bedragen als volgt vermeld:
-              </p>
-              <ul>
-                <li><span class="fw-semibold">Bruto Toeslag:</span> Bruto Toeslag</li>
-                <li><span class="fw-semibold">Vrijvalstaffel 1</span> Pensioentoeslag bas.</li>
-                <li><span class="fw-semibold">Vrijvalstaffel 2</span> Pensioentoeslag bov.</li>
-              </ul>
+            </p>
+            <ul>
+              <li><span class="fw-semibold">Bruto Toeslag:</span> Bruto Toeslag</li>
+              <li><span class="fw-semibold">Vrijvalstaffel 1</span> Pensioentoeslag bas.</li>
+              <li><span class="fw-semibold">Vrijvalstaffel 2</span> Pensioentoeslag bov.</li>
+            </ul>
           </div>
 
 
@@ -118,10 +126,11 @@
 </template>
 
 <script setup>
+import BigNumber from 'bignumber.js';
 import salarisTabel from '@/assets/salaris_tabel.json';
 import vrijvalStaffels from '@/assets/vrijvalstaffels.json';
 
-const max_employer_contribution = 137800;
+const max_employer_contribution = new BigNumber(137800);
 
 const formData = ref({
   salaryScale: 1,
@@ -130,80 +139,82 @@ const formData = ref({
   age: 21,
 });
 
-const calculatedContribution = ref(0);
-const brutoToeslag = ref(0);
-const netoBrutoToeslag = ref(0);
-const staffel_1 = ref(0);
-const staffel_2 = ref(0);
+const calculatedContribution = ref(new BigNumber(0));
+const brutoToeslag = ref(new BigNumber(0));
+const netoBrutoToeslag = ref(new BigNumber(0));
+const staffel_1 = ref(new BigNumber(0));
+const staffel_2 = ref(new BigNumber(0));
+const totalBruto = ref(new BigNumber(0));
+const resultsRef = ref(null);
 const showResults = ref(false);
-
 
 function getSalaryByFunctionAndScale(func, scale) {
   const scaleEntry = salarisTabel[scale.toString()];
   if (!scaleEntry) {
     console.error('Scale entry not found');
-    return 0; // Handle this case as you see fit
+    return new BigNumber(0);
   }
-
   const salaryString = func === 'FO' ? scaleEntry.fo : scaleEntry.cpt;
-  return parseFloat(salaryString.replace(',', '.')) * formData.value.employmentPercentage / 100;
+  return new BigNumber(salaryString.replace(',', '.')).multipliedBy(formData.value.employmentPercentage).dividedBy(100);
 }
 
 function getVrijvalStaffelsByAge(age) {
-  // Convert age to string to use as key, since your JSON keys are strings
   const ageKey = age.toString();
   const staffelsForAge = vrijvalStaffels[ageKey];
-
   if (!staffelsForAge) {
     console.error('No entry found for this age');
-    return {}; // Return an empty object or handle as you see fit
+    return {};
   }
-
   return staffelsForAge;
 }
 
-function calculateVrijval(monthly_salary, staffels) {
-  // Calculate vrijval based on salary and staffels
-  const retirement_giving_salary = monthly_salary * 12.83;
-  let vrijval_1;
-  // log vrijval1 to console and indicate what it is
-  console.log('vrijval_1:', vrijval_1);
-
-  let vrijval_2;
-  // if retirement giving salary more than max_employer_contribution
-  if (retirement_giving_salary > max_employer_contribution) {
-    vrijval_1 = (max_employer_contribution / 12) * staffels[1];
-    vrijval_2 = (retirement_giving_salary - max_employer_contribution) / 12 * staffels[2];
+function calculateVrijval(monthly_salary, staffels, retirement_giving_salary) {
+  let vrijval_1 = new BigNumber(0);
+  let vrijval_2 = new BigNumber(0);
+  if (retirement_giving_salary.isGreaterThan(max_employer_contribution)) {
+    vrijval_1 = max_employer_contribution.dividedBy(12).multipliedBy(staffels[1]);
+    vrijval_2 = retirement_giving_salary.minus(max_employer_contribution).dividedBy(12).multipliedBy(staffels[2]);
   } else {
-    vrijval_1 = retirement_giving_salary / 12 * staffels[1];
-    vrijval_2 = 0;
+    vrijval_1 = retirement_giving_salary.dividedBy(12).multipliedBy(staffels[1]);
+    vrijval_2 = new BigNumber(0);
   }
-  // log vrijval2 to console and indicate what it is
-  console.log('vrijval_2:', vrijval_2);
   staffel_1.value = vrijval_1;
   staffel_2.value = vrijval_2;
-  return vrijval_1 + vrijval_2;
+  return vrijval_1.plus(vrijval_2);
 }
+
 
 function calculateContribution() {
-  // Determine salary based on function and salaryScale
   const monthly_salary = getSalaryByFunctionAndScale(formData.value.function, formData.value.salaryScale);
-  console.log('salary:', monthly_salary);
-
+  const retirement_giving_salary = monthly_salary.multipliedBy(12.84);
   const staffels = getVrijvalStaffelsByAge(formData.value.age);
-  const vrijval = calculateVrijval(monthly_salary, staffels);
+  const vrijval = calculateVrijval(monthly_salary, staffels, retirement_giving_salary);
 
-  const bruto_contribution = ((monthly_salary * 12.83) / 12) * 0.073;
-  const completed_bruto_contribution = bruto_contribution * 11 / 7.3;
+  const bruto_contribution = retirement_giving_salary.dividedBy(12).multipliedBy(0.073);
+  const completed_bruto_contribution = bruto_contribution.multipliedBy(11).dividedBy(7.3);
   brutoToeslag.value = bruto_contribution;
   netoBrutoToeslag.value = completed_bruto_contribution;
-  // log bruto_contribution to console and indicate what it is
-  console.log('bruto_contribution:', bruto_contribution);
+  // Big number sum of netoBrutoToeslag and both vrijval
+  totalBruto.value = completed_bruto_contribution.plus(vrijval);
 
-  // Total net salary designated for retirement
-  calculatedContribution.value = (completed_bruto_contribution + vrijval) / 2;
+
+  calculatedContribution.value = completed_bruto_contribution.plus(vrijval).dividedBy(2);
   showResults.value = true;
+
+  nextTick(() => {
+    if (resultsRef.value) {
+      resultsRef.value.scrollIntoView({behavior: 'smooth'});
+    }
+  });
 }
+
+const formatNumber = (value) => {
+  // Ensure the value is a BigNumber instance
+  const bigNumberValue = BigNumber.isBigNumber(value) ? value : new BigNumber(value);
+
+  // Convert to a string with two decimal places
+  return bigNumberValue.toFixed(2);
+};
 </script>
 
 <style scoped>
